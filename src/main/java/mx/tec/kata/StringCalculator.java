9674bd7d8d5ead02c77309cb2643d69ab27e37b8
Alejandro Perez <alex98.perez@hotmail.com>
2021-04-09 17:47:22 -0600

    challenge 4

--- src/main/java/mx/tec/kata/StringCalculator.java
+++ src/main/java/mx/tec/kata/StringCalculator.java
@@ -2,23 +2,26 @@ package mx.tec.kata;
 
 public class StringCalculator {
 	
-	public int add(String numbers) {
-		
-		if(numbers.isBlank()) {
-			return 0;
-		}
-		
-		else {
-			String[] splitNumbers = numbers.split("\\n|,");
-			int sum = 0;
-			for(String numString: splitNumbers) {	
-				sum += Integer.parseInt(numString);
-			}
-			
-			return sum;	
-		}
-		
-		
-	}
+	public int add(String numbers){
+        String delimiter = ",";
+        if(numbers.isBlank()) {
+            return 0;
+        }
+        else{
+            String sanitizedNumbers = numbers;
+            if(numbers.startsWith("//")){
+                //extract delimiter
+                delimiter = numbers.substring(2,3);
+                //cut first three characters
+                sanitizedNumbers = numbers.substring(4);
+            }
+            String [] splitNumbers = sanitizedNumbers.split("\\n|" + delimiter);
+            int sum = 0;
+            for (String s: splitNumbers) {
+                sum += Integer.parseInt(s);
+            }
+            return  sum;
+        }
+    }
 
 }
